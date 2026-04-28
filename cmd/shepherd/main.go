6bc3d50c482b797dc0eb15f689ec45c9de490369
ne006 <IATikhomirov@gmail.com>
2026-04-28 20:25:50 +0300

    Redo cmd/shepherd/main.go using context like it was supposed to

--- cmd/shepherd/main.go
+++ cmd/shepherd/main.go
@@ -11,81 +11,47 @@ import (
 	"github.com/ne006/shepherd/supervisor"
 )
 
-type supervisorKey string
-
 func main() {
 	baseCtx := context.Background()
 	cancelCtx, cancel := context.WithCancel(baseCtx)
-	valueCtx := context.WithValue(cancelCtx, supervisorKey("supervisor"), supervisor.Supervisor{})
 
-	if err := trapSignals(valueCtx, cancel, runSupervisor, stopSupervisor); err != nil {
+	if err := trapSignals(cancelCtx, runSupervisor); err != nil {
 		fmt.Printf("Got error: %s", err)
+		cancel()
 		os.Exit(1)
 	} else {
+		cancel()
 		os.Exit(0)
 	}
 }
 
-func trapSignals(origCtx context.Context, cancel context.CancelFunc, runFn func(context.Context, context.CancelFunc), stopFn func(context.Context)) error {
+func trapSignals(origCtx context.Context, runFn func(context.Context)) error {
 	ctx, stop := signal.NotifyContext(origCtx, syscall.SIGINT, syscall.SIGTERM)
 
-	go runFn(ctx, cancel)
+	go runFn(ctx)
 
 	<-ctx.Done()
 
 	fmt.Println(ctx.Err())
-	stopFn(ctx)
 	stop()
 
 	return nil
 }
 
-func getSupervisor(ctx context.Context) (*supervisor.Supervisor, error) {
-
-	if v := ctx.Value(supervisorKey("supervisor")); v != nil {
-		if s, ok := v.(supervisor.Supervisor); ok {
-			return &s, nil
-		} else {
-			return nil, fmt.Errorf("Error initializing supervisor")
-		}
-	} else {
-		return nil, fmt.Errorf("Error initializing supervisor")
-	}
-}
-
-func runSupervisor(ctx context.Context, cancel context.CancelFunc) {
+func runSupervisor(ctx context.Context) {
 	var cl cli.CommandListener
 	var sv supervisor.Supervisor
-
-	if s, err := getSupervisor(ctx); err != nil {
-		fmt.Printf("%s\n", err)
-		cancel()
-		return
-	} else {
-		sv = *s
-	}
 
 	cl.Supervisor = &sv
 
 	if err := cl.Init(); err != nil {
 		fmt.Printf("Error initializing command listener: %s\n", err)
-		cancel()
 		return
 	} else {
 		go cl.Listen()
+
+		<-ctx.Done()
+
+		sv.Stop()
 	}
-}
-
-func stopSupervisor(ctx context.Context) {
-	var sv supervisor.Supervisor
-
-	if s, err := getSupervisor(ctx); err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	} else {
-		sv = *s
-	}
-
-	sv.Stop()
-
 }
